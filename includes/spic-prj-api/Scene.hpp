4c494791c1cf3ca2726afe97c7f9c2df1b45e0bf
Jaap <jaap@mrproper.dev>
2022-11-07 13:06:48 +0100

    Remove submodule

---
+++ includes/spic-prj-api/Scene.hpp
@@ -0,0 +1,28 @@
+#ifndef SCENE_H_
+#define SCENE_H_
+
+#include "GameObject.hpp"
+
+namespace spic {
+
+    /**
+     * @brief Class representing a scene which can be rendered by the Camera.
+     */
+    class Scene {
+        public:
+            /**
+             * @brief This function is called by a Camera to render the scene on the engine.
+             * @spicapi
+             */
+            void RenderScene();
+
+            /**
+             * @brief This property contains all the Game Object that are contained in this scene.
+             * @spicapi
+             */
+            std::vector<std::shared_ptr<GameObject>> contents;
+    };
+
+}
+
+#endif // SCENE_H_

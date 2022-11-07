3b1cf1618ee9ad5c92390e9644ec71b1f519a529
Jaap <jaap@mrproper.dev>
2022-11-07 15:58:18 +0100

    Fix window errors

--- src/Render/Window.cpp
+++ src/Render/Window.cpp
@@ -4,16 +4,15 @@
 #include "Render/Window.h"
 
 PlatformerEngine::Window::Window(int width, int height, const std::string &title, const spic::Color &color) {
-    graphicsFacade = GraphicsFacade();
-    graphicsFacade.init(width, height, title, color);
+    graphicsFacade.Init(width, height, title, color);
 }
 
 void PlatformerEngine::Window::Render() {
-    graphicsFacade.clear();
-    graphicsFacade.render();
+    graphicsFacade.Clear();
+    graphicsFacade.Render();
 }
 
 void PlatformerEngine::Window::Quit() {
-    graphicsFacade.quit();
+    graphicsFacade.Quit();
 }
 

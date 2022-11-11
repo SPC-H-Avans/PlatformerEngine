4f3dfceede92de85e972e51b0399ca89f6a61e64
Jesse <55792758+Nich-Thompson@users.noreply.github.com>
2022-11-11 13:05:24 +0100

    start api implementation

---
+++ src/API/Input.cpp
@@ -0,0 +1,23 @@
+#include <tuple>
+#include "Input.hpp"
+
+bool spic::Input::GetKey(spic::Input::KeyCode key) {
+    printf("joe");
+    return true;
+}
+
+//auto platformer_engine::Input::ListenForInput() -> std::vector<EventsEnum> {
+//    return _inputFacade.ListenForInput();
+//}
+//
+//auto platformer_engine::Input::IsKeyPressed(eKey key) -> bool {
+//    return InputFacade::IsKeyPressed(static_cast<InputFacade::eKey>(key));
+//}
+//
+//auto platformer_engine::Input::IsMouseButtonPressed(eMouseButton button) -> bool {
+//    return InputFacade::IsMouseButtonPressed(static_cast<InputFacade::eMouseButton>(button));
+//}
+//
+//auto platformer_engine::Input::GetMousePosition() -> std::tuple<int, int> {
+//    return InputFacade::GetMousePosition();
+//}
--- src/Render/Window.cpp
+++ src/Render/Window.cpp
@@ -1,11 +1,12 @@
 #include "Render/Window.hpp"
-#include "Input/Input.hpp"
+#include "../../includes/spic-prj-api/Input.hpp"
 
 platformer_engine::Window::Window(int width, int height, const std::string &title, const spic::Color &color) {
     _graphicsFacade.Init(width, height, title, color);
 }
 
 void platformer_engine::Window::Render() {
+    spic::Input::GetKey(spic::Input::KeyCode::A);
     _graphicsFacade.Clear();
     _graphicsFacade.Render();
 }

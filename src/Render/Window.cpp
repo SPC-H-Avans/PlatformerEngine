00d3a6af2c18b2519b9546dfb3da1f643cb66045
Wesley Salimans <wlt.salimans@student.avans.nl>
2022-11-14 14:01:06 +0100

    Update Window.cpp
    removed the temp code to render the tileset on screen

--- src/Render/Window.cpp
+++ src/Render/Window.cpp
@@ -5,21 +5,10 @@ platformer_engine::Window::Window(int width, int height, const std::string &titl
     _graphicsFacade = std::make_shared<GraphicsFacade>();
     _graphicsFacade->Init(width, height, title, color);
     TextureManager::GetInstance().Init(_graphicsFacade);
-
-    //TODO TEMP
-    LevelParser::GetInstance().ParseLevel("map1", "D:\\Avans\\Jaar 4\\SPC Project\\assets\\maps\\", "map.tmx");
-//    TextureManager::GetInstance().LoadTexture("tree", "D:\\Avans\\Jaar 4\\SPC Project\\assets\\tree.png");
 }
 
 void platformer_engine::Window::Render() {
     _graphicsFacade->Clear();
-
-    //TODO iets van gameobjecten plaatsen die gerenderd moeten worden?
-
-    //TODO TEMP
-//    _graphicsFacade->DrawTexture("tree", 0, 0, 500, 500);
-    LevelParser::GetInstance().GetLevel("map1")->Render();
-
 
     _graphicsFacade->Render();
 }

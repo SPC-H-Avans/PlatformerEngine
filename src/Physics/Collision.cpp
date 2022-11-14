6c26692f6fdf1ad189454cda5084cd52b0d8864a
Niels van Hal <ntj.vanhal@student.avans.nl>
2022-11-14 17:53:49 +0100

    fix error causing cmake crash

--- src/Physics/Collision.cpp
+++ src/Physics/Collision.cpp
@@ -1,12 +1,12 @@
 
-#include "Collision.hpp"
+#include "Physics/Collision.hpp"
 
 Collision::Collision(spic::Collider &other, CollisionPoint direction) : _other(other), _contact(std::make_unique<CollisionPoint>(direction)) {}
 Collision::Collision(spic::Collider &other) : _other(other), _contact(nullptr) {}
 
-auto Collision::Collider() const -> spic::Collider& { return _other; }
+auto Collision::GetCollider() const -> spic::Collider& { return _other; }
 
-void Collision::Collider(const spic::Collider &other) { this->_other = other; }
+void Collision::SetCollider(const spic::Collider &other) { this->_other = other; }
 
 auto Collision::Contact() const -> CollisionPoint& { return *_contact; }
 

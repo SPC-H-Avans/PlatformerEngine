04ba4535d88232429ed406b02ee5fbf738370979
Jesse <55792758+Nich-Thompson@users.noreply.github.com>
2022-11-14 11:46:16 +0100

    comments

--- src/Render/Window.cpp
+++ src/Render/Window.cpp
@@ -6,17 +6,24 @@ platformer_engine::Window::Window(int width, int height, const std::string &titl
 }
 
 void platformer_engine::Window::Render() {
-//    auto foo = spic::Input::MousePosition();
-//    SDL_Log("Mouse cursor is at %f, %f", foo.x, foo.y);
+//    // mouse position
+//    auto pos = spic::Input::MousePosition();
+//    SDL_Log("Mouse cursor is at %f, %f", pos.x, pos.y);
+//    // hold mouse
 //    if (spic::Input::GetMouseButton(spic::Input::MouseButton::RIGHT)) SDL_Log("Right");
-    if (spic::Input::GetKey(spic::Input::KeyCode::A)) SDL_Log("a");
-    if (spic::Input::GetKey(spic::Input::KeyCode::B)) SDL_Log("b");
+//    // hold valid key
+//    if (spic::Input::GetKey(spic::Input::KeyCode::A)) SDL_Log("a");
+//    if (spic::Input::GetKey(spic::Input::KeyCode::B)) SDL_Log("b");
+//    // hold invalid key
 //    if (spic::Input::GetKey(spic::Input::KeyCode::KEYPAD_00)) SDL_Log("keypad");
-    if (spic::Input::AnyKey()) SDL_Log("any");
-    if (spic::Input::GetMouseButtonDown(spic::Input::MouseButton::LEFT)) SDL_Log("m1 down");
-    if (spic::Input::GetMouseButtonUp(spic::Input::MouseButton::LEFT)) SDL_Log("m1 up");
-    if (spic::Input::GetMouseButtonDown(spic::Input::MouseButton::RIGHT)) SDL_Log("m2 down");
-    if (spic::Input::GetMouseButtonUp(spic::Input::MouseButton::RIGHT)) SDL_Log("m2 up");
+//    // hold any mouse/key
+//    if (spic::Input::AnyKey()) SDL_Log("any");
+//    // mouse up/down
+//    if (spic::Input::GetMouseButtonDown(spic::Input::MouseButton::LEFT)) SDL_Log("m1 down");
+//    if (spic::Input::GetMouseButtonUp(spic::Input::MouseButton::LEFT)) SDL_Log("m1 up");
+//    if (spic::Input::GetMouseButtonDown(spic::Input::MouseButton::RIGHT)) SDL_Log("m2 down");
+//    if (spic::Input::GetMouseButtonUp(spic::Input::MouseButton::RIGHT)) SDL_Log("m2 up");
+
     _graphicsFacade.Clear();
     _graphicsFacade.Render();
 }

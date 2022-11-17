0d3532488253820b9352897083070d4457735a0f
Wesley Salimans <wlt.salimans@student.avans.nl>
2022-11-17 15:52:21 +0100

    add namespace

--- includes/platformer-engine/Builder/SceneBuilder.hpp
+++ includes/platformer-engine/Builder/SceneBuilder.hpp
@@ -6,45 +6,47 @@
 #include "Camera.hpp"
 #include "Scene.hpp"
 
-class SceneBuilder {
-public:
-    /**
-     * @brief Constructor of Scene Builder
-     */
-    SceneBuilder();
+namespace platformer_engine {
+    class SceneBuilder {
+    public:
+        /**
+         * @brief Constructor of Scene Builder
+         */
+        SceneBuilder();
 
-    /**
-     * @brief Add a new Game Object to the scene
-     * @param gameObject Game Object to add to the scene
-     * @return SceneBuilder Modified Scene Builder
-     */
-    auto AddGameObject(const std::shared_ptr<spic::GameObject>& gameObject) -> SceneBuilder &;
+        /**
+         * @brief Add a new Game Object to the scene
+         * @param gameObject Game Object to add to the scene
+         * @return SceneBuilder Modified Scene Builder
+         */
+        auto AddGameObject(const std::shared_ptr<spic::GameObject> &gameObject) -> SceneBuilder &;
 
-    /**
-     * @brief Add a vector of Game Objects to the scene
-     * @param gameObjects Vector of Game Objects
-     * @return SceneBuilder Modified Scene Builder
-     */
-    auto AddGameObjects(const std::vector<std::shared_ptr<spic::GameObject>>& gameObjects) -> SceneBuilder &;
+        /**
+         * @brief Add a vector of Game Objects to the scene
+         * @param gameObjects Vector of Game Objects
+         * @return SceneBuilder Modified Scene Builder
+         */
+        auto AddGameObjects(const std::vector<std::shared_ptr<spic::GameObject>> &gameObjects) -> SceneBuilder &;
 
-    /**
-     * @brief Add a new Camera to the scene
-     * @param camera Camera to add to the scene
-     * @return SceneBuilder Modified Scene Builder
-     */
-    auto AddCamera(const std::shared_ptr<spic::Camera>& camera) -> SceneBuilder &;
+        /**
+         * @brief Add a new Camera to the scene
+         * @param camera Camera to add to the scene
+         * @return SceneBuilder Modified Scene Builder
+         */
+        auto AddCamera(const std::shared_ptr<spic::Camera> &camera) -> SceneBuilder &;
 
-    /**
-     * @brief Finalize the scene and retrieve it
-     * @return Scene Built scene
-     */
-    auto GetScene() -> std::unique_ptr<spic::Scene>;
+        /**
+         * @brief Finalize the scene and retrieve it
+         * @return Scene Built scene
+         */
+        auto GetScene() -> std::unique_ptr<spic::Scene>;
 
-private:
-    /**
-     * @brief Current scene object that is being build
-     */
-    std::unique_ptr<spic::Scene> _scene;
-};
+    private:
+        /**
+         * @brief Current scene object that is being build
+         */
+        std::unique_ptr<spic::Scene> _scene;
+    };
+}
 
 #endif //PLATFORMER_ENGINE_SCENEBUILDER_H

7a325212d1736e2c737ce2af79decb3c4555a2cd
Jaap <jaap@mrproper.dev>
2022-11-22 16:18:38 +0100

    Add enet

---
+++ includes/platformer-engine/Facade/NetworkingFacade.hpp
@@ -0,0 +1,9 @@
+#ifndef PLATFORMER_ENGINE_NETWORKINGFACADE_HPP
+#define PLATFORMER_ENGINE_NETWORKINGFACADE_HPP
+#include "enet/enet.h"
+namespace platformer_engine{
+    class NetworkingFacade{
+
+    };
+}
+#endif //PLATFORMER_ENGINE_NETWORKINGFACADE_HPP

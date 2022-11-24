735485ed3d31cd4d16444d47d83951cbc8bf601e
Jesse <55792758+Nich-Thompson@users.noreply.github.com>
2022-11-24 09:45:46 +0100

    added row and col to sprite

--- includes/spic-prj-api/Sprite.hpp
+++ includes/spic-prj-api/Sprite.hpp
@@ -16,7 +16,9 @@ namespace spic {
      */
     class Sprite : public Component {
     public:
-        Sprite(std::string spriteId, int sortingLayer, int orderInLayer, int spriteWidth, int spriteHeight, platformer_engine::SPIC_RendererFlip flip = platformer_engine::FLIP_NONE, Color color = Color::Transparent(), double spriteScale = 1.0);
+        Sprite(std::string spriteId, int sortingLayer, int orderInLayer, int spriteWidth, int spriteHeight,
+               platformer_engine::SPIC_RendererFlip flip = platformer_engine::FLIP_NONE, Color color = Color::Transparent(),
+               double spriteScale = 1.0, int spriteSheetRow = 0, int spriteSheetCol = 0);
         [[nodiscard]] inline auto GetSpriteId() const -> std::string {return _spriteId;};
         [[nodiscard]] inline auto GetSpriteScale() const -> double {return _spriteScale;};
 
@@ -26,11 +28,13 @@ namespace spic {
          */
         void Render(spic::Transform transform);
 
-        private:
+    private:
         Color _color;
-            platformer_engine::SPIC_RendererFlip _flip;
-            int _sortingLayer;
-            int _orderInLayer;
+        platformer_engine::SPIC_RendererFlip _flip;
+        int _sortingLayer;
+        int _orderInLayer;
+        int _spriteSheetRow;
+        int _spriteSheetCol;
 
     protected:
         std::string _spriteId; // NOLINT(cppcoreguidelines-non-private-member-variables-in-classes)
--- src/API/Sprite.cpp
+++ src/API/Sprite.cpp
@@ -2,12 +2,15 @@
 
 #include <utility>
 
-spic::Sprite::Sprite(std::string  spriteId, int sortingLayer, int orderInLayer, int spriteWidth,
-                     int spriteHeight, platformer_engine::SPIC_RendererFlip flip, spic::Color color, double spriteScale): _spriteId(std::move(spriteId)), _flip(flip), _sortingLayer(sortingLayer), _orderInLayer(orderInLayer), _spriteWidth(spriteWidth), _spriteHeight(spriteHeight),
-                                                     _color(color), _spriteScale(spriteScale) {
+spic::Sprite::Sprite(std::string  spriteId, int sortingLayer, int orderInLayer, int spriteWidth, int spriteHeight,
+                     platformer_engine::SPIC_RendererFlip flip, spic::Color color,
+                     double spriteScale, int spriteSheetRow, int spriteSheetCol) :
+        _spriteId(std::move(spriteId)), _sortingLayer(sortingLayer), _orderInLayer(orderInLayer), _spriteWidth(spriteWidth), _spriteHeight(spriteHeight),
+        _flip(flip), _color(color),
+        _spriteScale(spriteScale), _spriteSheetRow(spriteSheetRow), _spriteSheetCol(spriteSheetCol) {
 }
 
 void spic::Sprite::Render(spic::Transform transform) {
-   platformer_engine::TextureManager& textureManager = platformer_engine::TextureManager::GetInstance();
+    platformer_engine::TextureManager& textureManager = platformer_engine::TextureManager::GetInstance();
     textureManager.DrawTexture(_spriteId, transform.position.x, transform.position.y, _spriteWidth, _spriteHeight, _flip, _spriteScale);
 }

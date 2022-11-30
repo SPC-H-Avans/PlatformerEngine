5704c36fa68305d2cf873fc2a6c383b61bbf708e
KBelt <72886462+KBelt@users.noreply.github.com>
2022-11-30 09:27:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,10 +14,10 @@ auto main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) -> int {
     platformer_engine::SceneBuilder builder("Test Scene");
     auto scene = builder.GetScene();
     engine.AddScene(scene);
-//    engine.HostServer(scene.GetSceneName(), 10, 7779);
-//    engine.JoinServer("127.0.0.1", 7779);
-//    NetPkgs::Ping ping;
-//    engine.GetServerNetworkManager().SendUpdateToClients(&ping, sizeof(NetPkgs::Ping));
+    engine.HostServer(scene.GetSceneName(), 10, 7779);
+    engine.JoinServer("127.0.0.1", 7779);
+    NetPkgs::Ping ping;
+    engine.GetServerNetworkManager().SendUpdateToClients(&ping, sizeof(NetPkgs::Ping));
     engine.Start();
 
     std::cout << "Hello, World!" << std::endl;

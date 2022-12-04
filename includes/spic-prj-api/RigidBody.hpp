de485275a00181d5d1c84be8732bc34563eac515
Jaap <jaap@mrproper.dev>
2022-12-04 11:57:36 +0100

    Fix rigidbody

--- includes/spic-prj-api/RigidBody.hpp
+++ includes/spic-prj-api/RigidBody.hpp
@@ -6,6 +6,7 @@
 #include "Physics/Collision.hpp"
 #include <map>
 #include <boost/serialization/access.hpp>
+#include <boost/serialization/export.hpp>
 
 namespace spic {
 
@@ -22,35 +23,41 @@ namespace spic {
      * @brief A component representing a rigid body.
      */
     class RigidBody : public Component {
-        public:
+    public:
 
-        template<typename archive> void serialize(archive& ar, const unsigned /*version*/) {
+        template<typename archive>
+        void serialize(archive &ar, const unsigned /*version*/) {
+            ar & boost::serialization::base_object<Component, RigidBody>(*this);
+            boost::serialization::void_cast_register<RigidBody, Component>();
             ar & _mass;
             ar & _gravityScale;
             ar & _bodyType;
         }
 
-            /**
-             * @brief Apply force to this rigid body.
-             * @param forceDirection A point, used as a vector to indicate direction
-             *        and magnitude of the force to be applied.
-             * @spicapi
-             */
-            virtual void AddForce(const Point& forceDirection);
+        /**
+         * @brief Apply force to this rigid body.
+         * @param forceDirection A point, used as a vector to indicate direction
+         *        and magnitude of the force to be applied.
+         * @spicapi
+         */
+        virtual void AddForce(const Point &forceDirection);
 
-            void BodyType (BodyType bodyType) { this->_bodyType = bodyType; }
-            auto BodyType() -> enum BodyType { return _bodyType; }
+        void BodyType(BodyType bodyType) { this->_bodyType = bodyType; }
 
-            bool CanMoveTo(CollisionPoint point);
-            void AllowMoveTo(CollisionPoint point);
-            void DenyMoveTo(CollisionPoint point);
+        auto BodyType() -> enum BodyType { return _bodyType; }
 
-        protected:
-            float _mass;
-            float _gravityScale;
-            enum BodyType _bodyType;
+        bool CanMoveTo(CollisionPoint point);
 
-            std::map<CollisionPoint, int> _moveRestrictions;
+        void AllowMoveTo(CollisionPoint point);
+
+        void DenyMoveTo(CollisionPoint point);
+
+    protected:
+        float _mass;
+        float _gravityScale;
+        enum BodyType _bodyType;
+
+        std::map<CollisionPoint, int> _moveRestrictions;
     };
 
 } // namespace spic

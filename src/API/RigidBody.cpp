48962c130b7901599b098febca1543ccc49e1531
KBelt <72886462+KBelt@users.noreply.github.com>
2022-12-05 16:03:59 +0100

    Revert "Another remake of the physics system using only positive velocity"
    This reverts commit 4a49d3a7b515ad37559fa7b93ce15807bdf82f52.

--- src/API/RigidBody.cpp
+++ src/API/RigidBody.cpp
@@ -4,17 +4,17 @@
 
 void spic::RigidBody::AddForce(const spic::Point &force) {
 
-    Point velocity = {0,0};
+    SetHeading(Point::PointNormalize(force));
 
     auto x_acceleration = force.x / _mass;
-    velocity.x += x_acceleration;
+    _velocity.x += x_acceleration;
 
-    if (velocity.x > 0) {
-        velocity.x -= _friction;
-        velocity.x = std::min(velocity.x, _maxSpeed.x);
-    } else if (velocity.x < 0) {
-        velocity.x += _friction;
-        velocity.x = std::max(velocity.x, -_maxSpeed.x);
+    if (_velocity.x > 0) {
+        _velocity.x -= _friction;
+        _velocity.x = std::min(_velocity.x, _maxSpeed.x);
+    } else if (_velocity.x < 0) {
+        _velocity.x += _friction;
+        _velocity.x = std::max(_velocity.x, -_maxSpeed.x);
     }
 
     if (force.y > 0 &&
@@ -22,19 +22,15 @@ void spic::RigidBody::AddForce(const spic::Point &force) {
         || _gravityScale == 0)) { // Jump when on top of an object or if object has no gravity
 
         auto y_acceleration = force.y / _mass;
-        velocity.y -= y_acceleration;
+        _velocity.y -= y_acceleration;
     }
 
-    velocity.y += _gravityScale * _mass;
+    _velocity.y += _gravityScale * _mass;
 
-    if (velocity.y > 0 && !CanMoveTo(CollisionPoint::Bottom)) { velocity.y = 0; }
-    if (velocity.x > 0 && !CanMoveTo(CollisionPoint::Right)) { velocity.x = 0; }
-    if (velocity.x < 0 && !CanMoveTo(CollisionPoint::Left)) { velocity.x = 0; }
-    if (velocity.y < 0 && !CanMoveTo(CollisionPoint::Top)) { velocity.y = 0; }
-
-
-    SetHeading(Point::PointNormalize(velocity));
-    _velocity *= _heading;
+    if (_velocity.y > 0 && !CanMoveTo(CollisionPoint::Bottom)) { _velocity.y = 0; }
+    if (_velocity.x > 0 && !CanMoveTo(CollisionPoint::Right)) { _velocity.x = 0; }
+    if (_velocity.x < 0 && !CanMoveTo(CollisionPoint::Left)) { _velocity.x = 0; }
+    if (_velocity.y < 0 && !CanMoveTo(CollisionPoint::Top)) { _velocity.y = 0; }
 
     std::shared_ptr<GameObject> gameObject{GetGameObject().lock()};
     if (gameObject) {

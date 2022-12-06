1764deab8a442c9c2862e70097098475becdbe41
Jesse <55792758+Nich-Thompson@users.noreply.github.com>
2022-12-06 16:06:00 +0100

    bugfix
    i didnt realise scale also affected the initial coordinate

--- src/UI/ClickSystem.cpp
+++ src/UI/ClickSystem.cpp
@@ -18,8 +18,8 @@ void ClickSystem::Update() {
         if (button == nullptr) continue;
 
         // top left button position
-        auto btnXPos1 = button->GetTransform().position.x;
-        auto btnYPos1 = button->GetTransform().position.y;
+        auto btnXPos1 = button->GetTransform().position.x * button->GetSprite().GetSpriteScale();
+        auto btnYPos1 = button->GetTransform().position.y * button->GetSprite().GetSpriteScale();
         // bottom right button position
         auto btnXPos2 = btnXPos1 + button->GetWidth() * button->GetSprite().GetSpriteScale();
         auto btnYPos2 = btnYPos1 + button->GetHeight() * button->GetSprite().GetSpriteScale();

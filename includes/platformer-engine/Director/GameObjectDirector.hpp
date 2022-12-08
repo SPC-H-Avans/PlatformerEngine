b28eba7620f6e7ba81d8f440264637982e6efb0c
Jesse <55792758+Nich-Thompson@users.noreply.github.com>
2022-12-08 12:27:34 +0100

    restored documentation

--- includes/platformer-engine/Director/GameObjectDirector.hpp
+++ includes/platformer-engine/Director/GameObjectDirector.hpp
@@ -40,11 +40,13 @@ public:
     ) -> GameObject&;
 
     /**
-     * @brief Create a background object (not collidable) and add it to the scene
-     * @param sprite the sprite to use
+     * @brief Create a Player (collidable) and add it to the scene
+     * @param transform the transform of the Player
      * @param transform the transform to use
      * @param colliderWidth the width of the collider
      * @param colliderHeight the height of the collider
+     * @param animations a vector of AnimatedSprites to use for animations
+     * @param behaviourScripts a vector of BehaviourScripts that will apply to the player
      * @return The created GameObject
      */
     static auto CreatePlayer(

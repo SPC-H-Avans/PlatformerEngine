be3b188a002b5f996f6dcb4be761359afe5be641
Jaap <jaap@mrproper.dev>
2022-12-08 14:01:46 +0100

    Scene size

--- includes/platformer-engine/Networking/ProtocolDefinitions.hpp
+++ includes/platformer-engine/Networking/ProtocolDefinitions.hpp
@@ -28,7 +28,7 @@ constexpr int NET_ON_DISCONNECT = 2;
 
 
 constexpr int MAX_CREATE_GAME_OBJECT_SIZE = 2048;
-constexpr int MAX_CREATE_SCENE_SIZE = 102400;
+constexpr int MAX_CREATE_SCENE_SIZE = 269098;
 constexpr int MAX_SEND_CHARACTER_TO_SERVER_SIZE = 2048;
 constexpr int MAX_GAME_OBJECT_NAME_SIZE = 256;
 constexpr int MAX_ANIMATION_ID_SIZE = 256;

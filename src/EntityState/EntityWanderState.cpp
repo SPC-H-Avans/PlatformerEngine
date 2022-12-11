709e10c1ace58ce20ccffeef0167b17638d098d7
KBelt <72886462+KBelt@users.noreply.github.com>
2022-12-11 21:51:37 +0100

    Update EntityWanderState.cpp

--- src/EntityState/EntityWanderState.cpp
+++ src/EntityState/EntityWanderState.cpp
@@ -7,8 +7,23 @@ auto EntityWanderState::CalculateForce(std::shared_ptr<RigidBody> &rigidBody) ->
     if(!entityBody) {
         return {0,0};
     }
+    Point heading = entityBody->GetHeading();
+    Point velocity = entityBody->GetVelocity();
+    Point totalForce {0,0};
 
+    // Add wander force
+    totalForce += heading * velocity;
 
+    if(!entityBody->CanMoveTo(CollisionPoint::Right)) {
+        totalForce.x *= -1;
+    }
+
+    if(entityBody->GetHeading().Equals({0,0})) {
+        // Make sure the enemy is always heading somewhere
+        totalForce += {1,-1};
+    }
+
+    entityBody->AddForce(totalForce);
 }
 
 auto EntityWanderState::Clone() const -> std::unique_ptr<EntityState> {

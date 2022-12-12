29def803d7ced57afbb5d5d6939da6bdcb484f0c
Jesse <55792758+Nich-Thompson@users.noreply.github.com>
2022-12-12 16:21:41 +0100

    moved fpscounter to engine

---
+++ includes/platformer-engine/UI/FPSCounter.hpp
@@ -0,0 +1,22 @@
+#ifndef PLATFORMER_ENGINE_FPSCOUNTER_HPP
+#define PLATFORMER_ENGINE_FPSCOUNTER_HPP
+
+#include <memory>
+
+#include "GameObject.hpp"
+
+namespace platformer_engine {
+    class FPSCounter {
+    public:
+        /**
+         * @brief Constructor
+         */
+        FPSCounter(spic::Transform transform, std::string fontPath, int fontSize,
+                   spic::Color fontColor, int width, int height);
+
+    private:
+        std::shared_ptr<spic::GameObject> _obj;
+    };
+} // namespace platformer_engine
+
+#endif //PLATFORMER_ENGINE_FPSCOUNTER_HPP

5a8444f8bb2d5a35cb913c8e5cdad1a9c0334f67
KBelt <72886462+KBelt@users.noreply.github.com>
2022-12-15 11:44:20 +0100

    Moved functionality to the game

--- src/Physics/MoveSystem.cpp
+++ src/Physics/MoveSystem.cpp
@@ -62,17 +62,6 @@ auto MoveSystem::CalculateFrictionForce(const std::shared_ptr<RigidBody>& rigidB
 auto MoveSystem::CalculateFDEForce(const std::shared_ptr<GameObject>& gameObject) -> Point {
     auto forceDrivenEntity = std::dynamic_pointer_cast<platformer_engine::ForceDrivenEntity>(gameObject->GetComponent<platformer_engine::ForceDrivenEntity>());
     if(forceDrivenEntity != nullptr) {
-        auto following = forceDrivenEntity->GetFollowing().lock();
-        if(following) {
-            if(following->GetTransform().position.Distance(gameObject->GetTransform().position) < forceDrivenEntity->GetFollowRange()) {
-                forceDrivenEntity->FollowOn();
-            } else {
-                forceDrivenEntity->WanderOn();
-            }
-        } else {
-            forceDrivenEntity->WanderOn();
-            following.reset();
-        }
         auto FDEForce = forceDrivenEntity->CalcSteeringForce();
         return FDEForce;
     }
